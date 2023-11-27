99b8b44aaabfdcd976cd3c06256a5091f9b5a7b8
Aswad Tariq <aswadtariq@Aswads-MacBook-Air.local>
2023-11-27 17:32:33 -0500

    API finalized to work with recieving and sending Auth Scores

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const port = process.env.PORT || 3000;
 let authenticated = false;
 let lastAuthNanos = 0;
 
+const userScores = {}
+
 const gaitAuthDecisionHandler = decision => {
     pass
 }
@@ -21,8 +23,22 @@ app.get('/', (req, res) => {
 
 app.post('/send-gait-score', ({body}, res)=>{
     console.log(body)
+    const {authScore, username} = body
+    if (username in userScores) userScores[username] = [authScore]
+    else userScores[username].push(authScore)
+    console.log(userScores)
+
     //Receive the authenticator decision and forward that to our website
     res.send({'responseString': 'Ack'});
+})
+
+app.post('/get-gait-score', ({body}, res) => {
+    if (body.username in authScores) {
+        //Send most recent auth score
+        res.send({'authScore': authScores[username].slice(-1)})
+    }
+    res.send({'error': 'User with username does not have any auth score in our API'})
+
 })
 
 app.listen(port, '0.0.0.0', () => console.log(`Listening on port: ${port}`))
