138f0ea1289939c53f1b7426276c1f55f65a6582
Aswad Tariq <86660510+aswad546@users.noreply.github.com>
2023-11-27 17:00:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 
-const port = 3000
+const port = process.env.PORT || 3000;
+
 
 let authenticated = false;
 let lastAuthNanos = 0;
@@ -15,12 +16,12 @@ const gaitAuthDecisionHandler = decision => {
 }
 
 app.get('/', (req, res) => {
-    res.send('Welcome to SuperFido Auth API');
+    res.send('Welcome to SuperFido Auth API')
 })
 
-app.get('/gait-authenticator-decision', ({body}, res)=>{
+app.post('/send-gait-score', ({body}, res)=>{
     //Receive the authenticator decision and forward that to our website
     res.send('Ack');
 })
 
-app.listen(port, () => console.log(`Listening on port: ${port}`))
+app.listen(port, '0.0.0.0', () => console.log(`Listening on port: ${port}`))
